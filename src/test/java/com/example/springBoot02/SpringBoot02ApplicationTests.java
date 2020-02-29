eac5bb8e89b1b9f8c912c5eaf1fed552a689104a
zs <zs@163.com>
2020-02-29 15:16:29 +0800

    init project

---
+++ src/test/java/com/example/springBoot02/SpringBoot02ApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.springBoot02;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+
+@RunWith(SpringRunner.class) // 读取配置的东西
+@SpringBootTest
+
+public class SpringBoot02ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	
+	}
+
+	
+
+}
